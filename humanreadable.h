65847ea4ab0e1841cf6cb0a1c2c03f6218354039
Daniel Hannon <hello111f@gmail.com>
2024-01-13 12:44:22 +0000

    Added comparators for HumanReadable, need to implement subtraction

--- humanreadable.h
+++ humanreadable.h
@@ -5,12 +5,12 @@
  * Copyright: 2024 Daniel Hannon 
  */
 
-#include <cstddef>
 #ifndef HUMANREADABLE_H_2ADA56A63CC34EC5B844EF7F7C7178B5
 #define HUMANREADABLE_H_2ADA56A63CC34EC5B844EF7F7C7178B5 1
 #include <ostream>
 #include <vector>
 #include <cmath>
+#include <cstddef>
 
 /*
  * HUMAN READABLE NUMBER
@@ -26,6 +26,7 @@ struct HumanReadableNum {
 	{}
 
 	HumanReadableNum(int x): m_data{}, m_signed(std::signbit(x)) {
+		if(m_signed) x *= -1;
 		while(x != 0) {
 			m_data.emplace_back((x%10)+'0');
 			x/=10;
@@ -45,6 +46,16 @@ struct HumanReadableNum {
 		return os;	
 	}
 
+// Comparison Operators
+	friend bool operator==(HumanReadableNum const& lhs, HumanReadableNum const& rhs);
+	friend bool operator!=(HumanReadableNum const& lhs, HumanReadableNum const& rhs);
+	friend bool operator<(HumanReadableNum const& lhs, HumanReadableNum const& rhs);
+	friend bool operator>(HumanReadableNum const& lhs, HumanReadableNum const& rhs);
+	friend bool operator<=(HumanReadableNum const& lhs, HumanReadableNum const& rhs);
+	friend bool operator>=(HumanReadableNum const& lhs, HumanReadableNum const& rhs);
+
+
+// Arithmetic
 	/*
 	 * Add two HumanReadableNums together
 	 * TODO: Negative number handling (This will come with subtraction)
@@ -97,16 +108,88 @@ struct HumanReadableNum {
 	}
 
 	HumanReadableNum& operator-=(HumanReadableNum const& t) {
-		char tmp = 0;
-		char buff = 0;
-		
+		// TODO:Complete
+		char carry = 0;
+		char temp = 0;
+		std::size_t limit = std::min(m_data.size(),t.m_data.size());
+		for(std::size_t idx = 0; idx < limit; idx++) {
+			temp = m_data[idx];
+			m_data[idx] = subtract_fast(temp, t.m_data[idx], carry);
+			carry = temp < t.m_data[idx];
+		}
+		if(carry == 0) {
+			return *this;
+		}
+
 		return *this;
+	}
+
+private:
+	char subtract_fast(char a, char b, char carry) {
+		a -= '0';
+		b -= '0';
+		return (std::abs(a-b-carry)&0xFF) + '0';
 	}
 
 private:
 	std::vector<char> m_data;	// String digit data, in reverse order
 	bool			  m_signed; // If the number is signed and whatever
 };
+
+inline bool operator==(HumanReadableNum const& lhs, HumanReadableNum const& rhs) {
+	if(&lhs == &rhs) return true;
+
+	// TODO:Add handles to avoid -0
+	if(lhs.m_signed != rhs.m_signed) return false;
+
+	if(lhs.m_data.size() != rhs.m_data.size()) return false;
+
+	for(std::size_t idx = 0; idx < lhs.m_data.size(); idx++) {
+		if(lhs.m_data[idx] != rhs.m_data[idx]) return false;
+	}
+
+	return true;
+}
+
+inline bool operator!=(HumanReadableNum const& lhs, HumanReadableNum const& rhs) {
+	return !(lhs == rhs);
+}
+
+inline bool operator<(HumanReadableNum const& lhs, HumanReadableNum const& rhs) {
+	if(!lhs.m_signed && rhs.m_signed) {
+		return true;
+	} else if(lhs.m_signed && !rhs.m_signed) {
+		return false;
+	} else if(!lhs.m_signed && !rhs.m_signed) {
+		if(lhs.m_data.size() > rhs.m_data.size()) return true;
+		if(rhs.m_data.size() > lhs.m_data.size()) return false;
+		for(std::size_t idx = lhs.m_data.size() - 1; idx >= 1; idx--) {
+			if(lhs.m_data[idx] < rhs.m_data[idx]) return false;
+			if(lhs.m_data[idx] > rhs.m_data[idx]) return true;
+		}
+		return (lhs.m_data[0] > rhs.m_data[0]);
+	}
+	
+	if (lhs.m_data.size() < rhs.m_data.size()) return true;
+	if (lhs.m_data.size() > rhs.m_data.size()) return false;
+	for(std::size_t idx = lhs.m_data.size() - 1; idx >= 1; idx--) {
+		if(lhs.m_data[idx] > rhs.m_data[idx]) return false;
+		if(lhs.m_data[idx] < rhs.m_data[idx]) return true;
+	}
+	return (lhs.m_data[0] < rhs.m_data[0]);
+}
+
+inline bool operator<=(HumanReadableNum const& lhs, HumanReadableNum const& rhs) {
+	return (lhs<rhs) || (lhs==rhs);
+}
+
+inline bool operator>(HumanReadableNum const& lhs, HumanReadableNum const& rhs) {
+	return rhs < lhs;
+}
+
+inline bool operator>=(HumanReadableNum const& lhs, HumanReadableNum const& rhs) {
+	return (lhs > rhs) || (lhs == rhs);
+}
 
 #endif // HUMANREADABLE_H_2ADA56A63CC34EC5B844EF7F7C7178B5
 	
